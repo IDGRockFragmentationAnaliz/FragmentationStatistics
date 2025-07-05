10a7a279139428309ead2c90535635d1ebd14e5a
Kolad2 <koladik@gmail.com>
2025-07-05 05:00:47 +0300

    Commit 7

--- Task_plot_densities.py
+++ Task_plot_densities.py
@@ -11,19 +11,23 @@ def main():
     with open("./data/thin_section_densities.json") as file:
         thin_section_data = json.load(file)
     
+    with open("./data/outcrops_densities.json") as file:
+        outcrops_data = json.load(file)
+    
     fig = plt.figure(figsize=(12, 4))
     axs = [fig.add_subplot(1, 1, 1)]
     for name in zirkons_data:
-        x = np.array(zirkons_data[name]["s"])
-        y = np.array(zirkons_data[name]["rho"])
-        #print(zirkons_data[name]["unit"])
+        x = np.array(zirkons_data[name]["s"]) - 12
+        y = np.array(zirkons_data[name]["rho"]) + 2*12
         axs[0].plot(x, y, color="blue")
     for name in thin_section_data:
-        x = thin_section_data[name]["s"]
-        y = thin_section_data[name]["rho"]
-        #print(thin_section_data[name]["unit"])
+        x = np.array(thin_section_data[name]["s"]) - 12
+        y = np.array(thin_section_data[name]["rho"]) + 2*12
         axs[0].plot(x, y, color="red")
-    
+    for name in outcrops_data:
+        x = np.array(outcrops_data[name]["s"])
+        y = np.array(outcrops_data[name]["rho"])
+        axs[0].plot(x, y, color="red")
     plt.show()
 
 
