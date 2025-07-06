5e8679b8bbc0632d7c63ac2d56e7329445ac51a6
Kolad2 <koladik@gmail.com>
2025-07-07 01:36:49 +0300

    Commit 8

--- Task_plot_densities.py
+++ Task_plot_densities.py
@@ -1,10 +1,21 @@
+import os
 from pathlib import Path
+
+import math
 import numpy as np
-import json
+import scipy as sp
+import cv2
+import warnings
+
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+import json
 
 
 def main():
+    font_path = Path(".") / "assets" / "timesnewromanpsmt.ttf"
+    custom_font = mpl.font_manager.FontProperties(fname=font_path, size=16)
+    
     with open("./data/zirkons_densities.json") as file:
         zirkons_data = json.load(file)
     
@@ -13,21 +24,69 @@ def main():
     
     with open("./data/outcrops_densities.json") as file:
         outcrops_data = json.load(file)
+        
+    with open("./data/lineaments_densities.json") as file:
+        lineaments_densities = json.load(file)
     
-    fig = plt.figure(figsize=(12, 4))
+    fig = plt.figure(figsize=(12, 8))
+    colors = ["red", "blue", "purple", "green"]
+    
+    y_label_1 = r"Size-resolved number density, $\rho$, $m^{-4}$"
+    x_label_1 = "Particles area, $s$ $m^2$"
+    def get_line_config(i):
+        _config = {
+            "color": colors[i],
+            "linewidth": 3,
+            "solid_capstyle": 'round',
+            "solid_joinstyle": 'round'
+        }
+        return _config
+    
     axs = [fig.add_subplot(1, 1, 1)]
+    fig.subplots_adjust(bottom=0.2)
+    axs[0].grid(True)
     for name in zirkons_data:
         x = np.array(zirkons_data[name]["s"]) - 12
         y = np.array(zirkons_data[name]["rho"]) + 2*12
-        axs[0].plot(x, y, color="blue")
+        axs[0].plot(x, y, **get_line_config(0))
     for name in thin_section_data:
         x = np.array(thin_section_data[name]["s"]) - 12
         y = np.array(thin_section_data[name]["rho"]) + 2*12
-        axs[0].plot(x, y, color="red")
+        axs[0].plot(x, y, **get_line_config(1))
     for name in outcrops_data:
         x = np.array(outcrops_data[name]["s"])
         y = np.array(outcrops_data[name]["rho"])
-        axs[0].plot(x, y, color="red")
+        axs[0].plot(x, y, **get_line_config(2))
+    for name in lineaments_densities:
+        x = np.array(lineaments_densities[name]["s"])
+        y = np.array(lineaments_densities[name]["rho"])
+        axs[0].plot(x, y, **get_line_config(3))
+    axs[0].plot([10.5, -12], [-20, 25], color="black", linestyle="--", linewidth=3, dash_capstyle='round')
+    axs[0].set_xlabel(x_label_1, fontproperties=custom_font)
+    axs[0].set_ylabel(y_label_1, fontproperties=custom_font)
+    
+    def exp_formatter(x, pos):
+        return f'$10^{{{x:.0f}}}$'
+
+    def exp_formatter2(x, pos):
+        return f'$10^{{{x/2:.1f}}}$'
+    
+    ax2 = axs[0].twiny()
+    ax2.set_xlim(axs[0].get_xlim())
+    ax2.spines['bottom'].set_position(('outward', 60))
+    ax2.spines['bottom'].set_visible(True)
+    ax2.xaxis.set_ticks_position('bottom')
+    ax2.xaxis.set_label_position('bottom')
+    ax2.set_xlabel("Сharacteristic size, $d$ $m$", fontproperties=custom_font)
+    
+    axs[0].xaxis.set_major_formatter(plt.FuncFormatter(exp_formatter))
+    axs[0].yaxis.set_major_formatter(plt.FuncFormatter(exp_formatter2))
+    ax2.xaxis.set_major_formatter(plt.FuncFormatter(exp_formatter2))
+    for label in axs[0].get_xticklabels() + axs[0].get_yticklabels():
+        label.set_fontproperties(custom_font)
+    for label in ax2.get_xticklabels():
+        label.set_fontproperties(custom_font)
+    fig.savefig("./data/" + "all_stat_2" + ".png", dpi=300, bbox_inches='tight')
     plt.show()
 
 
